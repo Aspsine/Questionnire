d0cc4b6e61cb0de86e28cd4494226991b57d318a
Aspsine <littleximail@gmail.com>
2015-05-20 01:15:38 +0800

    click sort

--- app/src/main/java/com/fang/chinaindex/questionnaire/ui/adapter/OptionDragSortAdapter.java
+++ app/src/main/java/com/fang/chinaindex/questionnaire/ui/adapter/OptionDragSortAdapter.java
@@ -1,7 +1,5 @@
 package com.fang.chinaindex.questionnaire.ui.adapter;
 
-import android.media.Image;
-import android.support.v4.view.ViewCompat;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,12 +8,7 @@ import android.widget.TextView;
 
 import com.fang.chinaindex.questionnaire.R;
 import com.fang.chinaindex.questionnaire.model.Option;
-import com.h6ah4i.android.widget.advrecyclerview.draggable.DraggableItemAdapter;
-import com.h6ah4i.android.widget.advrecyclerview.draggable.ItemDraggableRange;
-import com.h6ah4i.android.widget.advrecyclerview.draggable.RecyclerViewDragDropManager;
 import com.h6ah4i.android.widget.advrecyclerview.utils.AbstractDraggableItemViewHolder;
-
-import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,77 +16,36 @@ import java.util.List;
 /**
  * Created by Aspsine on 2015/5/19.
  */
-public class OptionDragSortAdapter extends RecyclerViewAdapter implements DraggableItemAdapter<RecyclerView.ViewHolder> {
+public class OptionDragSortAdapter extends RecyclerViewAdapter {
     private static final String TAG = OptionDragSortAdapter.class.getSimpleName();
 
     private List<Option> mOptions;
 
     public OptionDragSortAdapter() {
         mOptions = new ArrayList<Option>();
-        // DraggableItemAdapter requires stable ID, and also
-        // have to implement the getItemId() method appropriately.
-        setHasStableIds(true);
     }
 
     public void setData(List<Option> options) {
         mOptions.clear();
         mOptions.addAll(options);
-        notifyDataSetChanged();
-    }
-
-    public static boolean hitTest(View v, int x, int y) {
-        final int tx = (int) (ViewCompat.getTranslationX(v) + 0.5f);
-        final int ty = (int) (ViewCompat.getTranslationY(v) + 0.5f);
-        final int left = v.getLeft() + tx;
-        final int right = v.getRight() + tx;
-        final int top = v.getTop() + ty;
-        final int bottom = v.getBottom() + ty;
-
-        return (x >= left) && (x <= right) && (y >= top) && (y <= bottom);
-    }
-
-    @Override
-    public boolean onCheckCanStartDrag(RecyclerView.ViewHolder viewHolder, int position, int x, int y) {
-        // x, y --- relative from the itemView's top-left
-        DragSortViewHolder holder = (DragSortViewHolder) viewHolder;
-        final View containerView = holder.itemContainer;
-        final View dragHandleView = holder.ivDragHandler;
-
-        final int offsetX = containerView.getLeft() + (int) (ViewCompat.getTranslationX(containerView) + 0.5f);
-        final int offsetY = containerView.getTop() + (int) (ViewCompat.getTranslationY(containerView) + 0.5f);
-
-        return hitTest(dragHandleView, offsetX, offsetY);
-    }
-
-    @Override
-    public ItemDraggableRange onGetItemDraggableRange(RecyclerView.ViewHolder viewHolder, int i) {
-        final int start = 0;
-        final int end = getDividePosition();
-
-        return new ItemDraggableRange(start, end);
-    }
-
-    public int getDividePosition() {
         int i = 0;
-        while (i < mOptions.size() && mOptions.get(i).isChecked()) {
-            i++;
+        for (Option option : mOptions) {
+            option.setOptionTitle(i++ + "");
         }
-        return i;
-    }
-
-    @Override
-    public void onMoveItem(int fromPosition, int toPosition) {
-        if (fromPosition == toPosition) {
-            return;
-        }
-        final Option option = mOptions.remove(fromPosition);
-        mOptions.add(toPosition, option);
-        notifyItemMoved(fromPosition, toPosition);
+        notifyDataSetChanged();
     }
 
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int i) {
-        return new DragSortViewHolder(inflate(parent, R.layout.item_option_sort));
+        final DragSortViewHolder holder = new DragSortViewHolder(inflate(parent, R.layout.item_option_sort));
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int position = holder.getLayoutPosition();
+                setChecked(position);
+            }
+        });
+        return holder;
     }
 
     @Override
@@ -106,20 +58,36 @@ public class OptionDragSortAdapter extends RecyclerViewAdapter implements Dragga
         return mOptions.size();
     }
 
-    public void setChecked(int position, boolean isChecked) {
+    public void setChecked(int position) {
         Option option = mOptions.get(position);
+        boolean isChecked = option.isChecked();
         int divPos = getDividePosition();
-        option.setChecked(isChecked);
+
+        option.setChecked(!isChecked);
         mOptions.remove(option);
+//        notifyItemRemoved(position);
         if (isChecked) {
-            mOptions.add(divPos, option);
-        } else {
-            mOptions.add(divPos - 1, option);
+            divPos = divPos - 1;
         }
-        notifyDataSetChanged();
+        mOptions.add(divPos, option);
+//        notifyItemInserted(divPos);
+        notifyItemMoved(position, divPos);
+        notifyItemRangeChanged(position <= divPos ? position : divPos, Math.abs(divPos - position) + 1);
     }
 
-    public static class DragSortViewHolder extends AbstractDraggableItemViewHolder implements View.OnClickListener {
+    public interface PositionChangedCallBack {
+        void changed(int divPos);
+    }
+
+    public int getDividePosition() {
+        int i = 0;
+        while (i < mOptions.size() && mOptions.get(i).isChecked()) {
+            i++;
+        }
+        return i;
+    }
+
+    public static class DragSortViewHolder extends AbstractDraggableItemViewHolder {
         ViewGroup itemContainer;
         ImageView ivDragHandler;
         TextView tvTitle;
@@ -132,7 +100,6 @@ public class OptionDragSortAdapter extends RecyclerViewAdapter implements Dragga
             ivDragHandler = (ImageView) itemView.findViewById(R.id.ivDragHandle);
             tvTitle = (TextView) itemView.findViewById(R.id.tvTitle);
             tvNum = (TextView) itemView.findViewById(R.id.tvNum);
-            itemView.setOnClickListener(this);
         }
 
         public void bindData(Option option) {
@@ -141,32 +108,6 @@ public class OptionDragSortAdapter extends RecyclerViewAdapter implements Dragga
             tvNum.setVisibility(mOption.isChecked() ? View.VISIBLE : View.GONE);
             tvTitle.setText(mOption.getOptionTitle());
             tvNum.setText(String.valueOf(getLayoutPosition()));
-
-            final int dragState = getDragStateFlags();
-
-            if ((dragState & RecyclerViewDragDropManager.STATE_FLAG_IS_UPDATED) != 0) {
-                Integer bgResId = 0;
-                if ((dragState & RecyclerViewDragDropManager.STATE_FLAG_IS_ACTIVE) != 0) {
-                    //dragging
-                    bgResId = R.drawable.bg_option_item_selector;
-                } else if (((dragState & RecyclerViewDragDropManager.STATE_FLAG_DRAGGING) != 0) &&
-                        ((dragState & RecyclerViewDragDropManager.STATE_FLAG_IS_IN_RANGE) != 0)) {
-                    //dragging others' state
-                    bgResId = R.drawable.bg_option_item_selector;
-                } else {
-                    //normal
-                    bgResId = R.drawable.bg_option_item_selector;
-                }
-                itemContainer.setBackgroundResource(bgResId);
-            }
-
-        }
-
-        @Override
-        public void onClick(View v) {
-            mOption.setChecked(!mOption.isChecked());
-            tvNum.setVisibility(mOption.isChecked() ? View.VISIBLE : View.GONE);
-            ivDragHandler.setVisibility(mOption.isChecked() ? View.VISIBLE : View.GONE);
         }
     }
 
--- app/src/main/java/com/fang/chinaindex/questionnaire/ui/fragment/SortFragment.java
+++ app/src/main/java/com/fang/chinaindex/questionnaire/ui/fragment/SortFragment.java
@@ -1,22 +1,15 @@
 package com.fang.chinaindex.questionnaire.ui.fragment;
 
 
-import android.os.Build;
-import android.os.Bundle;
 import android.app.Fragment;
+import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 
 import com.fang.chinaindex.questionnaire.R;
-import com.fang.chinaindex.questionnaire.model.Question;
 import com.fang.chinaindex.questionnaire.ui.adapter.OptionDragSortAdapter;
-import com.h6ah4i.android.widget.advrecyclerview.animator.GeneralItemAnimator;
-import com.h6ah4i.android.widget.advrecyclerview.animator.RefactoredDefaultItemAnimator;
-import com.h6ah4i.android.widget.advrecyclerview.draggable.RecyclerViewDragDropManager;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -25,10 +18,6 @@ public class SortFragment extends QuestionBaseFragment {
 
     private RecyclerView recyclerView;
     private OptionDragSortAdapter mAdapter;
-    RecyclerViewDragDropManager mDragDropManager;
-
-    private RecyclerView.Adapter mWrappedAdapter;
-
 
     public SortFragment() {
         // Required empty public constructor
@@ -37,6 +26,7 @@ public class SortFragment extends QuestionBaseFragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mAdapter = new OptionDragSortAdapter();
     }
 
     @Override
@@ -47,28 +37,10 @@ public class SortFragment extends QuestionBaseFragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        mDragDropManager = new RecyclerViewDragDropManager();
         recyclerView = (RecyclerView) view.findViewById(R.id.recyclerView);
-        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
         recyclerView.setLayoutManager(layoutManager);
-//        layoutManager.setDraggingItemShadowDrawable();
-
-        final OptionDragSortAdapter adapter = new OptionDragSortAdapter();
-        mAdapter = adapter;
-        mWrappedAdapter = mDragDropManager.createWrappedAdapter(adapter);
-        final GeneralItemAnimator animator = new RefactoredDefaultItemAnimator();
-
-        recyclerView.setAdapter(mAdapter); // requires *wrapped* adapter
-        recyclerView.setItemAnimator(animator);
-        // additional decorations
-        //noinspection StatementWithEmptyBody
-        if (supportsViewElevation()) {
-            // Lollipop or later has native drop shadow feature. ItemShadowDecorator is not required.
-        }else {
-//            recyclerView.addItemDecoration();
-        }
-
-//        mDragDropManager.attachRecyclerView(recyclerView);
+        recyclerView.setAdapter(mAdapter);
     }
 
     @Override
@@ -80,10 +52,6 @@ public class SortFragment extends QuestionBaseFragment {
     @Override
     public void onPause() {
         super.onPause();
-        mDragDropManager.cancelDrag();
     }
 
-    private boolean supportsViewElevation() {
-        return (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP);
-    }
 }
