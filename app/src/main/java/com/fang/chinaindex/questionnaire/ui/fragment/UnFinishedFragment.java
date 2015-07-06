dc2171b5002cb6c78f74f33ba4ceeb313c8ca6d3
Aspsine <littleximail@gmail.com>
2015-07-06 17:48:51 +0800

    ActionMode

--- app/src/main/java/com/fang/chinaindex/questionnaire/ui/fragment/UnFinishedFragment.java
+++ app/src/main/java/com/fang/chinaindex/questionnaire/ui/fragment/UnFinishedFragment.java
@@ -2,24 +2,25 @@ package com.fang.chinaindex.questionnaire.ui.fragment;
 
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.text.TextUtils;
+import android.view.ActionMode;
+import android.view.ContextMenu;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 
 import com.fang.chinaindex.questionnaire.App;
 import com.fang.chinaindex.questionnaire.R;
 import com.fang.chinaindex.questionnaire.model.SurveyInfo;
-import com.fang.chinaindex.questionnaire.ui.activity.SurveyActivity;
 import com.fang.chinaindex.questionnaire.ui.adapter.RecyclerViewAdapter;
 import com.fang.chinaindex.questionnaire.ui.adapter.UnfinishedAdapter;
-import com.fang.chinaindex.questionnaire.util.L;
 import com.fang.chinaindex.questionnaire.util.SharedPrefUtils;
 
 import java.util.List;
@@ -44,6 +45,7 @@ public class UnFinishedFragment extends Fragment implements RecyclerViewAdapter.
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
         mAdapter = new UnfinishedAdapter();
         mAdapter.setOnItemClickListener(this);
         mAdapter.setOnItemLongClickListener(this);
@@ -65,38 +67,79 @@ public class UnFinishedFragment extends Fragment implements RecyclerViewAdapter.
     }
 
     @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater inflater = getActivity().getMenuInflater();
+        inflater.inflate(R.menu.menu_login, menu);
+    }
+
+    @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         refresh();
     }
 
     @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        return super.onContextItemSelected(item);
+
+    }
+
+    @Override
     public void onItemClick(View v, int position, SurveyInfo surveyInfo) {
-        if (mEditMode) {
-            surveyInfo.setSelected(!surveyInfo.isSelected());
-            mAdapter.notifyItemChanged(position);
-        } else {
-            Intent intent = new Intent(getActivity(), SurveyActivity.class);
-            intent.putExtra("EXTRA_SURVEY_ID", String.valueOf(surveyInfo.getSurveyId()));
-            intent.putExtra("EXTRA_SURVEY_START_TIME", surveyInfo.getStartTime());
-            v.getContext().startActivity(intent);
-        }
+//        if (mEditMode) {
+//            surveyInfo.setSelected(!surveyInfo.isSelected());
+//            mAdapter.notifyItemChanged(position);
+//        } else {
+//            Intent intent = new Intent(getActivity(), SurveyActivity.class);
+//            intent.putExtra("EXTRA_SURVEY_ID", String.valueOf(surveyInfo.getSurveyId()));
+//            intent.putExtra("EXTRA_SURVEY_START_TIME", surveyInfo.getStartTime());
+//            v.getContext().startActivity(intent);
+//        }
+        showEditModeToolBar();
     }
 
     @Override
     public boolean onItemLongClick(View v, int position, SurveyInfo surveyInfo) {
-        if (!mEditMode) {
-            mEditMode = true;
-            showEditModeToolBar();
-            mAdapter.setEditMode(true);
-            onItemClick(v, position, surveyInfo);
-            return false;
-        }
+//        if (!mEditMode) {
+//            showEditModeToolBar();
+//            return false;
+//        }
         return true;
     }
 
-    private void showEditModeToolBar() {
 
+    private ActionMode.Callback mActionModeCallBack = new ActionMode.Callback() {
+        @Override
+        public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+            MenuInflater inflater = mode.getMenuInflater();
+            inflater.inflate(R.menu.menu_login, menu);
+            return true;
+        }
+
+        @Override
+        public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+            return false;
+        }
+
+        @Override
+        public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+            if (item.getItemId() == R.id.action_settings) {
+                mode.finish();
+                return true;
+            }
+            return false;
+        }
+
+        @Override
+        public void onDestroyActionMode(ActionMode mode) {
+
+        }
+    };
+
+
+    private void showEditModeToolBar() {
+        getActivity().startActionMode(mActionModeCallBack);
     }
 
     private void refresh() {
